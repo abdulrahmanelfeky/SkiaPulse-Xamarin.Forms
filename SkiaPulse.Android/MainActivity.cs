2f45cd7d7e7dd658b69be2f908963f15b7519395
abdulrahman elfeky <abdulrahmanelfeky@abdulrahmans-MacBook-Pro.local>
2019-03-20 11:26:45 +0300

    nothing

--- SkiaPulse.Android/MainActivity.cs
+++ SkiaPulse.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace SkiaPulse.Droid
 {
-    [Activity(Label = "SkiaPulse", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SkiaPulse ", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
